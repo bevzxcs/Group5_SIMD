29bee8d4c16b2e1ab6000f929c69e1fc49ae0d8e
bevzxcs <97271510+bevzxcs@users.noreply.github.com>
2024-06-03 20:28:21 +0800

    Update asmfunc1.asm
    draft 1

--- SIMD/asmfunc1.asm
+++ SIMD/asmfunc1.asm
@@ -1,3 +1,5 @@
+;DRAFT ONLY
+
 section .data
 
 section .text
@@ -8,6 +10,44 @@ global nonsimd_stencil
 extern printf
 
 nonsimd_stencil:
-	
+    ; Prologue
+    push rbp
+    mov rbp, rsp
 
-	ret
+    ; Function parameters:
+    ; rdi - pointer to X
+    ; rsi - pointer to Y
+    ; rdx - length of the vector n
+
+    mov r8, rdx          ; Store length of vector n in r8
+    sub r8, 3            ; Calculate n - 3
+
+    mov rcx, 3           ; Start index i from 3
+
+.loop:
+    ; Check if we've reached the end of the array (i >= n - 3)
+    cmp rcx, r8
+    jge .done
+
+    ; Perform the stencil operation:
+    ; Y[i] = X[i-3] + X[i-2] + X[i-1] + X[i] + X[i+1] + X[i+2] + X[i+3]
+    mov eax, [rdi + 4*rcx - 12]  ; X[i-3]
+    add eax, [rdi + 4*rcx - 8]   ; X[i-2]
+    add eax, [rdi + 4*rcx - 4]   ; X[i-1]
+    add eax, [rdi + 4*rcx]       ; X[i]
+    add eax, [rdi + 4*rcx + 4]   ; X[i+1]
+    add eax, [rdi + 4*rcx + 8]   ; X[i+2]
+    add eax, [rdi + 4*rcx + 12]  ; X[i+3]
+
+    ; Store the result in Y[i]
+    mov [rsi + 4*rcx], eax
+
+    ; Increment the index
+    inc rcx
+    jmp .loop
+
+.done:
+    ; Epilogue
+    mov rsp, rbp
+    pop rbp
+    ret
